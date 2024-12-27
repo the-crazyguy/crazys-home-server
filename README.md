6ec82504d2d5315e294418f9b20cb118e700d401
Dimitar Dyulgerov <mitko937@gmail.com>
2024-12-27 23:33:20 +0100

    Add basic download functionality

--- README.md
+++ README.md
@@ -2,7 +2,35 @@
 
 Home server for file storage and private web hosting
 
+## Structure
+
+It is important to note that the project should follow a client-server structure
+instead of a monolithic one. This is done to separate the concerns for the server
+and the client.
+
+By adding authentication and authorization to the application, its complexity grows.
+The client will serve as the front-end. It will:
+- Provide users with a UI to fill in forms
+- Store authentication keys provided by the server
+- Make calls to the server
+
+The server, on the other hand, will be a RESTful API server. It will:
+- Provide an endpoint for logging in (POST)
+    - Generating tokens, etc.
+- Provide endpoints for uploading files (POST)
+- Provide endpoints for downloading files (GET)
+
+The client will still technically be a *server*, however it will simply route the
+user's requests to the back-end (the RESTful API server)
+
+
 ## References
 
 - [Go/Gin getting started docs](https://go.dev/doc/tutorial/web-service-gin)
 - [Gin upload docs](https://gin-gonic.com/docs/examples/upload-file/multiple-file/)
+- [Gin static files](https://chroniconl.vercel.app/articles/serving-static-content-with-go-and-gin)
+
+## Resources to look up later
+
+- [Go REST client, with appended Authorization header](https://dev.to/der_gopher/writing-rest-api-client-in-go-3fkg#:~:text=req.Header.Set(%22Authorization%22%2C%20fmt.Sprintf(%22Bearer%20%25s%22%2C%20c.apiKey)))
+- [Go Authentication methods by JetBrains](https://www.jetbrains.com/guide/go/tutorials/authentication-for-go-apps/auth/)
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,10 +18,116 @@ func main() {
 	// TODO: Add an environment variable/pass a variable for the on-system filepath
 	// router.Static("/files", "./user-files")
 
+	// TODO: Use templates
 	router.StaticFile("/", "./public/index.html")
+	router.StaticFile("/login", "./public/login.html")
+
+	router.POST("/login-form", postLoginForm)
 	router.POST("/upload", postUpload)
+	router.GET("/download/:filename", authnMiddlewareMock, postDownload)
 
 	router.Run("localhost:8080")
+}
+
+func authnMiddlewareMock(c *gin.Context) {
+	c.Set("username", "crzy")
+	c.Next()
+}
+
+func authnMiddleware(c *gin.Context) {
+	// Step 1: Get the Authorization header
+	authHeader := c.GetHeader("Authorization")
+	if authHeader == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+		log.Println("Error: No authorization header")
+		c.Abort()
+		return
+	}
+
+	// Step 2: Extract the token string
+	tknStr := strings.Split(authHeader, " ")[1]
+	// TODO: REMOVE, here for testing
+	log.Println(tknStr)
+	// Step 3: Parse the token (i.e. with jwt)
+
+	// Step 4: Check for token validity
+
+	// Step 5: Extract useful information from claims i.e. Username
+
+	// Step 6: Set the extracted information in the context (c)
+
+	// Step 7: Continue to next handler
+	c.Next()
+}
+
+func postDownload(c *gin.Context) {
+	// Step 1: Get username from context
+	username, exists := c.Get("username")
+
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Username missing from context"})
+		log.Println("Error: Username missing from context")
+		c.Abort()
+		return
+	}
+	// Step 2: Get filename from URL (or rework to a POST request w/ JSON?)
+	fileName := c.Param("filename")
+
+	if fileName == "" {
+		c.JSON(http.StatusNotFound, gin.H{"error": "filename is empty"})
+		log.Println("Error: filename missing from url")
+		c.Abort()
+		return
+	}
+
+	// Step 3: Construct filepath
+	path := filepath.Join("user-files", username.(string), filepath.Clean(fileName))
+
+	// WARN: Test for path traversal!
+	if !strings.HasPrefix(path, "user-files") {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Access denied"})
+		log.Printf("Error: access to %s is denied\n", path)
+		c.Abort()
+		return
+	}
+
+	// Step 4: Check if the path exists
+	if _, err := os.Stat(path); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "file not found"})
+		log.Printf("Error: file %s not found\n", path)
+		c.Abort()
+		return
+	}
+
+	// Step 5: Serve the file
+	// TODO: Test if this supports range requests
+	// TODO: Test with large files
+	c.FileAttachment(path, fileName)
+
+	// Alternatively: Serve via http.SetveContent to handle range requests if c.FileAttachment doesn't work
+	// http.ServeContent(c.Writer, c.Request, fi)
+}
+
+func postLogin(c *gin.Context) {
+	// Step 1: Get params from provided json (username, password)
+	// Step 2: Validate username and password
+	// Step 3: Generate JWT/Bearer token
+	// Step 4: Return bearer token as a response
+}
+
+func postLoginForm(c *gin.Context) {
+	var u struct {
+		Username string `form:"username" binding:"required"`
+	}
+	if err := c.ShouldBind(&u); err != nil {
+		c.String(http.StatusBadRequest, "Binding error: %s", err.Error())
+		log.Println("Binding error: ", err.Error())
+
+		return
+	}
+
+	log.Println("Username: ", u.Username)
+	c.JSON(http.StatusOK, gin.H{"message": "Login successful", "user": u})
 }
 
 // TODO: Make it so each separate file upload is a separate POST request
@@ -31,13 +139,13 @@ func postUpload(c *gin.Context) {
 	desc := c.PostForm("description")
 
 	// debug
-	fmt.Println("Owner: ", owner)
-	fmt.Println("Description: ", desc)
+	log.Println("Owner: ", owner)
+	log.Println("Description: ", desc)
 
 	form, err := c.MultipartForm()
 	if err != nil {
 		c.String(http.StatusBadRequest, "Form error: %s", err.Error())
-		fmt.Println("Form error: ", err.Error())
+		log.Println("Form error: ", err.Error())
 
 		return
 	}
@@ -45,7 +153,7 @@ func postUpload(c *gin.Context) {
 	files := form.File["files"]
 	if len(files) == 0 {
 		c.String(http.StatusBadRequest, "No files to upload.")
-		fmt.Println("No files to upload")
+		log.Println("No files to upload")
 
 		return
 	}
@@ -54,7 +162,7 @@ func postUpload(c *gin.Context) {
 
 	if err := os.MkdirAll(uploadDir, os.ModePerm); err != nil {
 		c.String(http.StatusInternalServerError, "Directory creation error: %s", err.Error())
-		fmt.Println("Directory creation error: ", err.Error())
+		log.Println("Directory creation error: ", err.Error())
 	}
 
 	for _, f := range files {
@@ -64,7 +172,7 @@ func postUpload(c *gin.Context) {
 
 		if err := c.SaveUploadedFile(f, uploadPath); err != nil {
 			c.String(http.StatusBadRequest, "Upload file error: %s", err.Error())
-			fmt.Println("Upload file error: ", err.Error())
+			log.Println("Upload file error: ", err.Error())
 
 			return
 		}
