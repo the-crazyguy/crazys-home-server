7adcf1e75d29262bdbfe392d957594a270637783
Dimitar Dyulgerov <mitko937@gmail.com>
2024-12-28 06:34:16 +0100

    Retrieve username as string

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -14,18 +14,24 @@ func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	router := gin.Default()
+	// TODO: Remove once done w/ testing
+	unsecure := router.Group("/unsecure", authnMiddlewareMock)
 	// TODO: Max multipart memory
 
 	// TODO: Add an environment variable/pass a variable for the on-system filepath
 	// router.Static("/files", "./user-files")
 
-	// TODO: Use templates
+	// TODO: Remove, here for quick testing
 	router.StaticFile("/", "./public/index.html")
-	router.StaticFile("/login", "./public/login.html")
+	// router.StaticFile("/login", "./public/login.html")
 
-	router.POST("/login-form", postLoginForm)
-	router.POST("/upload", postUpload)
-	router.GET("/download/:filename", authnMiddlewareMock, getDownload)
+	// router.POST("/login-form", postLoginForm)
+
+	router.POST("/form-upload", postFormUpload)
+	unsecure.POST("/form-upload", postFormUpload)
+
+	router.GET("/download/:filename", authnMiddleware, getDownload)
+	unsecure.GET("/download/:filename", getDownload)
 
 	router.Run("localhost:8080")
 }
@@ -63,14 +69,14 @@ func authnMiddleware(c *gin.Context) {
 
 func getDownload(c *gin.Context) {
 	// Step 1: Get username from context
-	username, exists := c.Get("username")
-
-	if !exists {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Username missing from context"})
-		log.Println("Error: Username missing from context")
+	owner := c.GetString("username")
+	if owner == "" {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "username is empty or not found"})
+		log.Println("Error: username is empty or not found")
 		c.Abort()
 		return
 	}
+
 	// Step 2: Get filename from URL (or rework to a POST request w/ JSON?)
 	fileName := c.Param("filename")
 
@@ -82,7 +88,7 @@ func getDownload(c *gin.Context) {
 	}
 
 	// Step 3: Construct filepath
-	path := filepath.Join("user-files", username.(string), filepath.Clean(fileName))
+	path := filepath.Join("user-files", owner, filepath.Clean(fileName))
 
 	// WARN: Test for path traversal!
 	if !strings.HasPrefix(path, "user-files") {
@@ -131,12 +137,23 @@ func postLoginForm(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Login successful", "user": u})
 }
 
-// TODO: Make it so each separate file upload is a separate POST request
+// TODO:
+// 1. Make it so each separate file upload is a separate POST request
 // so you can give each file a description. Would require the creation of a
 // File storage/access api/db
+// 2. Handle same-named files
+//   - append a (n), where n is the smallest number possible (i.e. file(1), file(2), etc.)
+//   - append current date to the file name and strip it when serving it (I like this better)
+func postFormUpload(c *gin.Context) {
+	// owner := c.PostForm("owner-name")	// Depricated
+	owner := c.GetString("username")
+	if owner == "" {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "username is empty or not found"})
+		log.Println("Error: username is empty or not found")
+		c.Abort()
+		return
+	}
 
-func postUpload(c *gin.Context) {
-	owner := c.PostForm("owner-name")
 	desc := c.PostForm("description")
 
 	// debug
